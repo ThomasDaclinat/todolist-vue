6f358615fadd2ca6c3f91d783483ad174b187898
Thomas Daclinat <derklein@macbook-pro-de-thomas.home>
2024-10-01 06:19:00 +0200

    added css-doodle background

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,43 @@
 <template>
-  <h1>TodoList-Vue</h1>
-
-  <form action="" @submit.prevent="addTodo">
-    <fieldset role="group">
-      <input v-model="newTodo" type="text" placeholder="Tâche à effectuer">
-      <button :disabled="newTodo.length == 0">Ajouter</button>
-    </fieldset>
-  </form>
-  <label>
-    <input type="checkbox" v-model="hideCompleted">
-    Masquer les tâches complétées
-  </label>
-  <p v-if="remainingTodos > 0">Il reste {{ remainingTodos }} tâche{{ remainingTodos > 1 ? 's' : '' }} à compléter</p>
-  <hr>
-  <div v-if="todos.length === 0">Aucune tâche à effectuer</div>
-  <div v-else>
-    <ul>
-      <li v-for="todo in sortedTodos" :key="todo.date" :class="{ completed: todo.completed }">
-        <label>
-          <input type="checkbox" v-model="todo.completed">
-          {{ todo.title }}
-        </label>
-      </li>
-    </ul>
-  </div>
+  <main class="scroll-container">
+    <h1>TodoList-Vue</h1>
+    <form action="" @submit.prevent="addTodo">
+      <fieldset role="group">
+        <input v-model="newTodo" type="text" placeholder="Tâche à effectuer">
+        <button :disabled="newTodo.length == 0">Ajouter</button>
+      </fieldset>
+    </form>
+    <label>
+      <input type="checkbox" v-model="hideCompleted">
+      Masquer les tâches complétées
+    </label>
+    <p v-if="remainingTodos > 0">Il reste {{ remainingTodos }} tâche{{ remainingTodos > 1 ? 's' : '' }} à compléter</p>
+    <hr>
+    <div v-if="todos.length === 0">Aucune tâche à effectuer</div>
+    <div v-else>
+      <ul>
+        <li v-for="todo in sortedTodos" :key="todo.date" :class="{ completed: todo.completed }">
+          <label>
+            <input type="checkbox" v-model="todo.completed">
+            {{ todo.title }}
+          </label>
+        </li>
+      </ul>
+    </div>
+  </main>
+  <css-doodle>
+    :doodle {
+    @grid: 7 / 100vmax / #0a0c27;
+    position: absolute;
+    top: 0; left: 0;
+    z-index: -1;
+    }
+    @shape: clover 5;
+    background: hsla(-@i(*4), 70%, 68%, @r.8);
+    transform:
+    scale(@r(.2, 1.5))
+    translate(@m2.@r(±50%));
+  </css-doodle>
 </template>
 
 <script setup>
